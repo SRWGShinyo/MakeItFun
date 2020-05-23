f6c60af025c932250fea5e176424f5470d91f2ff
Yohann DE <yohann.degli-esposti@epita.fr>
2020-05-24 00:43:51 +0200

    main title: pimp my title.

---
+++ Assets/Scripts/MainTitle/PoofDrapeauOnRandom.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoofDrapeauOnRandom : MonoBehaviour
+{
+    public float beBackAfter = 4f;
+    bool isAway = false;
+    float randomAfterGo;
+    public GameObject whereToSpawn;
+    public GameObject poof;
+
+    AudioSource away;
+    private void Start()
+    {
+        randomAfterGo = Random.Range(4f, 9f);
+        away = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isAway)
+        {
+            if (randomAfterGo < 0)
+            {
+                disappear();
+                isAway = true;
+                randomAfterGo = Random.Range(4f, 9f);
+                return;
+            }
+
+            randomAfterGo -= Time.deltaTime;
+        }
+
+        else
+        {
+            if (beBackAfter < 0)
+            {
+                appear();
+                isAway = false;
+                beBackAfter = Random.Range(4f, 9f);
+            }
+
+            beBackAfter -= Time.deltaTime;
+        }
+    }
+
+    private void disappear()
+    {
+        foreach (Transform tr in transform)
+        {
+            if (tr.gameObject.GetComponent<MeshRenderer>())
+                tr.gameObject.GetComponent<MeshRenderer>().enabled = false;
+        }
+
+        Instantiate(poof, whereToSpawn.transform.position, whereToSpawn.transform.rotation);
+        away.Stop();
+        away.Play();
+    }
+
+    private void appear()
+    {
+        foreach (Transform tr in transform)
+        {
+            if (tr.gameObject.GetComponent<MeshRenderer>())
+                tr.gameObject.GetComponent<MeshRenderer>().enabled = true;
+        }
+        Instantiate(poof, whereToSpawn.transform.position, whereToSpawn.transform.rotation);
+        away.Stop();
+        away.Play();
+    }
+}
---
+++ Assets/Scripts/MainTitle/RobotMadness.cs
@@ -0,0 +1,58 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotMadness : MonoBehaviour
+{
+    Animator anim;
+    float reopen;
+    float ball;
+
+    public GameObject start;
+    public GameObject goTo;
+    bool isBall = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        ball = Random.Range(4, 9);
+        reopen = Random.Range(4, 9);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isBall)
+        {
+            if (ball < 0)
+            {
+                StartCoroutine(move());
+            }
+            ball -= Time.deltaTime;
+        }
+
+        else
+        {
+            if (reopen < 0)
+            {
+                anim.SetBool("Roll_Anim", false);
+                isBall = false;
+                transform.DOMove(start.transform.position, 1f);
+                reopen = Random.Range(4, 9);
+            }
+
+            reopen -= Time.deltaTime;
+        }
+    }
+
+    private IEnumerator move()
+    {
+        anim.SetBool("Roll_Anim", true);
+        yield return new WaitForSeconds(2.4f);
+        isBall = true;
+        ball = Random.Range(4, 9);
+        transform.DOMove(goTo.transform.position, 1f);
+    }
+}
