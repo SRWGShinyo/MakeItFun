045fc0510780ba1727bc4a3da626e46ba0407ceb
Yohann DE <yohann.degli-esposti@epita.fr>
2020-05-23 11:47:59 +0200

    pause: added a pause menu.

---
+++ Assets/Scripts/PauseMenuFunctions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuFunctions : MonoBehaviour
+{
+    public GameObject panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !panel.activeSelf)
+        {
+            if (GameObject.FindGameObjectWithTag("Player"))
+                GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<FPSCameraLook>().enabled = false;
+
+            if (GameObject.FindGameObjectWithTag("Flag") && GameObject.FindGameObjectWithTag("Flag").GetComponentInChildren<FPSCameraLook>() != null)
+                GameObject.FindGameObjectWithTag("Flag").GetComponentInChildren<FPSCameraLook>().enabled = false;
+
+            Cursor.lockState = CursorLockMode.None;
+            panel.SetActive(true);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Escape) && panel.activeSelf)
+        {
+            if (GameObject.FindGameObjectWithTag("Player"))
+                GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<FPSCameraLook>().enabled = true;
+
+            if (GameObject.FindGameObjectWithTag("Flag") && GameObject.FindGameObjectWithTag("Flag").GetComponentInChildren<FPSCameraLook>() != null)
+                GameObject.FindGameObjectWithTag("Flag").GetComponentInChildren<FPSCameraLook>().enabled = true;
+
+            Cursor.lockState = CursorLockMode.Locked;
+            panel.SetActive(false);
+        }
+    }
+}
